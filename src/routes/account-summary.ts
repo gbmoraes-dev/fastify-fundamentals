985b7f53a69ed255dae521dfdffebc8c5cbadb32
gbmoraes-dev <gbmoraes.dev@gmail.com>
2024-11-12 00:43:55 -0300

    feat(transactions): add session ID check to account summary route

--- src/routes/account-summary.ts
+++ src/routes/account-summary.ts
@@ -2,12 +2,23 @@ import type { FastifyInstance } from 'fastify'
 
 import { knex } from '../database'
 
-export function accountSummary(app: FastifyInstance) {
-	app.get('/transactions/summary', async () => {
-		const summary = await knex('transactions')
-			.sum('amount', { as: 'amount' })
-			.first()
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
-		return summary
-	})
+export function accountSummary(app: FastifyInstance) {
+	app.get(
+		'/transactions/summary',
+		{
+			preHandler: [checkSessionIdExists],
+		},
+		async (request) => {
+			const { sessionId } = request.cookies
+
+			const summary = await knex('transactions')
+				.where('session_id', sessionId)
+				.sum('amount', { as: 'amount' })
+				.first()
+
+			return summary
+		},
+	)
 }
