048f1c4d7c441db3dbabc192a37789aee878db23
gbmoraes-dev <gbmoraes.dev@gmail.com>
2024-11-12 16:20:16 -0300

    feat(app): separate app and server for testing

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+
+import cookie from '@fastify/cookie'
+
+import {
+	accountSummary,
+	createTransaction,
+	getTransactionById,
+	listTransactions,
+} from './routes'
+
+export const app = fastify()
+
+app.register(cookie)
+
+app
+	.register(createTransaction)
+	.register(listTransactions)
+	.register(getTransactionById)
+	.register(accountSummary)
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,6 @@
-import fastify from 'fastify'
-
-import cookie from '@fastify/cookie'
+import { app } from './app'
 
 import { env } from './env'
-
-import {
-	accountSummary,
-	createTransaction,
-	getTransactionById,
-	listTransactions,
-} from './routes'
-
-const app = fastify()
-
-app.register(cookie)
-
-app
-	.register(createTransaction)
-	.register(listTransactions)
-	.register(getTransactionById)
-	.register(accountSummary)
 
 app
 	.listen({
