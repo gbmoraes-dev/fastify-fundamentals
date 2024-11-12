0e521d37bee57fb1a75af49dfa0dde58a38f39f6
gbmoraes-dev <gbmoraes.dev@gmail.com>
2024-11-12 00:39:43 -0300

    feat(transactions): add session ID check to get transaction by id route

--- src/routes/get-transaction-by-id.ts
+++ src/routes/get-transaction-by-id.ts
@@ -4,16 +4,31 @@ import { z } from 'zod'
 
 import { knex } from '../database'
 
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+
 const getTransactionParamsSchema = z.object({
 	id: z.string().uuid(),
 })
 
 export function getTransactionById(app: FastifyInstance) {
-	app.get('/transactions/:id', async (request) => {
-		const { id } = getTransactionParamsSchema.parse(request.params)
+	app.get(
+		'/transactions/:id',
+		{
+			preHandler: [checkSessionIdExists],
+		},
+		async (request) => {
+			const { id } = getTransactionParamsSchema.parse(request.params)
 
-		const transaction = await knex('transactions').where('id', id).first()
+			const { sessionId } = request.cookies
 
-		return { transaction }
-	})
+			const transaction = await knex('transactions')
+				.where({
+					id,
+					session_id: sessionId,
+				})
+				.first()
+
+			return { transaction }
+		},
+	)
 }
